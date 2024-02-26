94151b1b30ce6b54ae60158ceb9eb70b406be770
DavidLoveSpace <davidlovespace@crc-dot1x-nat-10-239-206-91.bu.edu>
2024-02-26 10:43:44 -0500

    [styling] adding some Todo for cleaner code

--- src/pages/mentorProfile.tsx
+++ src/pages/mentorProfile.tsx
@@ -6,11 +6,11 @@ function MentorProfile(){
             <div className="main">
                 <div className="profile">
                     {/* Nguyen Le */}
-                    <div className="generalInfo">
+                    <div className="generalInfo"> {/* instead of using each class for each section, use a single container for all*/}
                         <div className="pfp">
                             <img src="./src/assets/alt_img/no_image.jpg"></img>
                         </div>
-                        <div className="emptySpace"></div>
+                        <div className="emptySpace"></div> {/* Don't do this*/}
                         <div className="attributes">
                             <div className="name">Mathew Nunez</div>
                             <div className="nationality">
@@ -21,7 +21,7 @@ function MentorProfile(){
                                 <div className="logo">
                                     <img src="./src/assets/bu.svg"></img>
                                 </div>
-                                <div className="eduinfo">
+                                <div className="eduinfo"> {/* two of these are off centered*/}
                                     <div className="school">Boston University</div>
                                     <div className="gradYear">Class of 2027</div>
                                 </div>
@@ -30,9 +30,9 @@ function MentorProfile(){
 
                     </div>
 
-                    <div className="profileOverview">
+                    <div className="profileOverview"> {/* this is the same as "reviews" class, maybe combine two of them into one*/}
                         <div className="title">Overview</div>
-                        <div className="description">
+                        <div className="description"> {/*this is also similar same as "topics" class, maybe combine them*/}
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </div>
                         <div className="topics">I am mentoring in Arts and Science</div>
