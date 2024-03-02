1e02c1f7400783fce5bec88314b886d6562e0be9
Bendinh <bendinh0805@gmail.com>
2024-03-02 15:53:49 -0500

    [Filter] Migrate fetch mentor attributes from filter component to mentor display page

--- src/components/filter.tsx
+++ src/components/filter.tsx
@@ -1,66 +1,72 @@
 import './filter.css'
-import { list_mentors } from '../backend/firebase';
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { FilterProps } from '../types/filterProps';
 
 // Card component to display user profile and attributes
-function Filter() {
-  const [mentors, setMentors] = useState<any []>([]);
-  const [topics, setTopics] = useState<string[]>([]);
-  const [majors, setMajors] = useState<string[]>([]);
-  const [nationalities, setNationalities] = useState<string[]>([]);
-  const [schools, setSchools] = useState<string[]>([]);
+function Filter({filterCriteria}: {filterCriteria: FilterProps}) {
+    const {topics, majors, nationalities, schools} = filterCriteria
+    // const [selectedTopic, setSelectedTopic] = useState<string>('*')
+    // const [selectedMajor, setSelectedMajor] = useState<string>('*')
+    // const [selectedNationality, setSelectedNationality] = useState<string>('*')
+    // const [selectedSchool, setSelectedSchool] = useState<string>('*')
 
-  useEffect(() => {
-    const fetch_mentors = async() => {
-      const mentors  = await list_mentors();
-      setMentors(mentors);
+//   useEffect(() => {
+//     // Filter mentors based on selected criteria
+//     const filterMentors = () => {
+//         let result = Array.isArray(filterCriteria) ? filterCriteria : [filterCriteria];
 
-      // Extract unique topics, majors, nationalities, and schools
+//       if (selectedTopic) {
+//         result = result.filter(mentor => mentor.topics.includes(selectedTopic));
+//       }
+//       if (selectedMajor) {
+//         result = result.filter(mentor => mentor.major === selectedMajor);
+//       }
+//       if (selectedNationality) {
+//         result = result.filter(mentor => mentor.nationality === selectedNationality);
+//       }
+//       if (selectedSchool) {
+//         result = result.filter(mentor => mentor.school === selectedSchool);
+//       }
 
-      const topics = [...new Set(mentors.flatMap((mentor) => (mentor.topics)))];
-      const majors = [...new Set(mentors.map((mentor) => (mentor.major)))];
-      const nationalities = [...new Set(mentors.map((mentor) => (mentor.nationality)))];
-      const schools = [...new Set(mentors.map(mentor => mentor.school))];
+//       setFilteredMentors(result);
+//     };
 
-      // Update state
-      setTopics(topics);
-      setMajors(majors);
-      setNationalities(nationalities);
-      setSchools(schools);
-    }
-
-    fetch_mentors();
-  }, [])
+//     filterMentors();
+//   }, [selectedTopic, selectedMajor, selectedNationality, selectedSchool, mentors]);
 
   return (
     <div className='container'>
         <select>
+        <option value="*">Topic of Interest</option>
         {topics.map((topic, index) => (
-            <option key={index}>
+            <option key={index} value={topic}>
             {topic}
             </option>
         ))}
         </select>
 
         <select>
+        <option value="*">Major</option>
         {majors.map((major, index) => (
-            <option key={index}>
+            <option key={index} value={major}>
             {major}
             </option>
         ))}
         </select>
 
         <select>
+        <option value="*">Nationaility</option>
         {nationalities.map((nationality, index) => (
-            <option key={index}>
+            <option key={index} value={nationality}>
             {nationality}
             </option>
         ))}
         </select>
 
         <select>
+        <option value="*">School</option>
         {schools.map((school, index) => (
-            <option key={index}>
+            <option key={index} value={school}>
             {school}
             </option>
         ))}
--- src/pages/Mentors.tsx
+++ src/pages/Mentors.tsx
@@ -4,15 +4,28 @@ import Filter from '../components/filter';
 import { useState, useEffect } from "react";
 import { list_mentors } from '../backend/firebase';
 import { Mentor } from '../types/mentor';
+import { FilterProps } from '../types/filterProps';
 
 export const Mentors = () => {
     
-    const [fetchedMentors, setMentors] = useState<{ id: string; }[]>([]);
+    const [fetchedMentors, setMentors] = useState<Mentor[]>([]);
+    const [filterCriteria, setFilterCriteria] = useState<FilterProps>({topics: [], majors: [], nationalities: [], schools: []})
   
     useEffect(() => {
     const fetch_mentors = async() => {
       const fetchedMentors  = await list_mentors()
-      setMentors(fetchedMentors)
+      setMentors(fetchedMentors as Mentor[])
+
+      // Extract unique topics, majors, nationalities, and schools
+      const filterCriteria = {
+        topics: [...new Set(fetchedMentors.flatMap((mentor) => (mentor.topics)))],
+        majors: [...new Set(fetchedMentors.map((mentor) => (mentor.major)))],
+        nationalities: [...new Set(fetchedMentors.map((mentor) => (mentor.nationality)))],
+        schools: [...new Set(fetchedMentors.map(mentor => mentor.school))],
+      }
+
+    // Update state
+      setFilterCriteria(filterCriteria)
     };
     
       fetch_mentors();
@@ -21,7 +34,7 @@ export const Mentors = () => {
     return (
         <div>
             <h1 id = "intro-container">Meet with your mentors!</h1>
-            <Filter />
+            <Filter filterCriteria={filterCriteria} />
             <Card mentors={fetchedMentors as Mentor[]} />
         </div>
     );
