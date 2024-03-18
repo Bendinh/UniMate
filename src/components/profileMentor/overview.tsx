aea4ca3c5fb9304001543322787753c177fac850
FilipLe <48429871+FilipLe@users.noreply.github.com>
2024-03-17 22:55:24 -0400

    [fix]: clean code after compatibility bug

--- src/components/profileMentor/overview.tsx
+++ src/components/profileMentor/overview.tsx
@@ -6,12 +6,12 @@ function Overview(){
         <div className='main'>
         <div className="foreground"></div>
             <div className='profile'>
-                <div className="generalInfo"> {/* instead of using each class for each section, use a single container for all*/}
+                <div className="generalInfo"> 
                     <div className="pfp">
                         
                         <img src="./src/assets/alt_img/no_image.jpg"></img>
                     </div>
-                    <div className="emptySpace"></div> {/* Don't do this*/}
+                    <div className="emptySpace"></div> 
                     <div className="attributes">
                         <div className="name">Mathew Nunez</div>
                         <div className="nationality">
@@ -22,7 +22,7 @@ function Overview(){
                             <div className="logo">
                                 <img src="./src/assets/bu.svg"></img>
                             </div>
-                            <div className="eduinfo"> {/* two of these are off centered*/}
+                            <div className="eduinfo"> 
                                 <div className="school">Boston University</div>
                                 <div className="gradYear">Class of 2027</div>
                             </div>
@@ -33,9 +33,9 @@ function Overview(){
 
                 </div>
 
-                <div className="profileOverview"> {/* this is the same as "reviews" class, maybe combine two of them into one*/}
+                <div className="profileOverview"> 
                     <div className="title">Overview</div>
-                    <div className="description"> {/*this is also similar same as "topics" class, maybe combine them*/}
+                    <div className="description"> 
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </div>
                     <div className="topics">I am mentoring in Arts and Science</div>
