6fdc82b3e8364b7e22bf9a982e115a55c8a86b1d
Bendinh <bendinh0805@gmail.com>
2024-03-24 17:49:43 -0400

    [Debug] Fix bugs affecting deployment regarding typing mismatch in overview

--- src/services/firebase.tsx
+++ src/services/firebase.tsx
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, getDocs, getDoc,  collection, doc, setDoc, query, where } from "firebase/firestore";
+import { getFirestore, getDocs, getDoc,  collection, doc, query, where } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
