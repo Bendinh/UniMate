323e3aa798bf315c2fcda617a83dc3b0d4e1180d
DavidLoveSpace <davidlovespace@crc-dot1x-nat-10-239-80-149.bu.edu>
2024-03-24 18:03:00 -0400

    [feat] add extra sample description for mentor profile

--- src/components/profileMentor/overview.tsx
+++ src/components/profileMentor/overview.tsx
@@ -61,7 +61,7 @@ function Overview() {
                     <h3>Overview</h3>
                 </div>
                 <div className='col description'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...
+                   {mentor?.description}
                     <div className='show'>show more</div>
                 </div>
                 <div className='col topics'>
