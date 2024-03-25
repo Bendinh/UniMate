4bc04eadda4ba8124d2e573097fc02d97f59b9c1
Bendinh <bendinh0805@gmail.com>
2024-03-25 12:55:15 -0400

    [UserAuth] Add logout function

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -4,13 +4,19 @@ import "../../styles/navbar.css";
 import logo from "../../assets/logo/unimate_logo.png";
 import { useRef } from 'react';
 import {auth} from "../../services/firebase"
-import {onAuthStateChanged } from "firebase/auth";
-import {Avatar} from "@mui/material"
-import {Menu, AccountCircle} from '@mui/icons-material';
+import {onAuthStateChanged, signOut } from "firebase/auth";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuIcon from '@mui/icons-material/Menu'; 
 
 export const Navbar = () => {
     const [isAuthenticated,setIsAuthenticated] = useState(false);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
     const [name,setName] = useState("");
+    // const [dropdownOpen, setDropdownOpen] = useState(false);
     const menu = useRef<HTMLDivElement>(null);
     const location = useLocation();
     const hideNavbarPaths = ["/login", "/signup", "/signup1", "/signup2"];
@@ -26,6 +32,26 @@ export const Navbar = () => {
         }
     }
 
+    const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            setIsAuthenticated(false);
+            handleClose();
+        } catch (error) {
+            console.error("Logout Error:", error);
+        }
+    };
+
+    // const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
     useEffect (() => {
         onAuthStateChanged(auth, (user) => {
             if (user) {
@@ -38,7 +64,6 @@ export const Navbar = () => {
         });
     });
 
-    
     // function stringToColor(string: string) {
     //     let hash = 0;
     //     let i;
@@ -68,7 +93,7 @@ export const Navbar = () => {
     // }
 
     if (hideNavbarPaths.includes(location.pathname)) {
-        return null; // Do not render the navbar for the specified paths
+        return null;
     }
     
     return (
@@ -82,19 +107,44 @@ export const Navbar = () => {
                 <div className='tab'><Link to='/about'>ABOUT</Link></div>
             </div>
             <div className='item login'>
-                {isAuthenticated ? (
-                    <div>
-                        {/*<Avatar {...stringAvatar(name)}/>*/}
-                        <AccountCircle sx={{marginTop: '40%'}}/>
-                    </div>
-                ) : (
-                <>
-                    <Link to='/login'>LOGIN</Link>
-                </>
-                )}
+                        {isAuthenticated ? (
+                            <div>
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleMenu}
+                                    color="inherit"
+                                >
+                                    <AccountCircle />
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorEl}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={open}
+                                    onClose={handleClose}
+                                >
+                                    <MenuItem onClick={handleClose}>{name}</MenuItem>
+                                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                                </Menu>
+                            </div>
+                        ) : (
+                            <Link to='/login'>LOGIN</Link>
+                        )}
                 </div>
             <div className='item-v2'>
-                <div className='burger' onClick={openMenu}><Menu/></div>
+                <div className='burger' onClick={openMenu}><MenuIcon /></div>
             </div>
         </div>
         {/*Menu for mobile*/}
